7eac837e5c78f8871f38b3832899c4237d12dac2
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-14 18:28:28 +0000

    8/13 working. Put things in wrong place.

---
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -0,0 +1,11 @@
+const Park = function(name, ticket_price, dinosaur_collection){
+    this.name = name;
+    this.ticket_price = ticket_price;
+    this.dinosaur_collection = dinosaur_collection
+}
+
+
+
+
+
+module.exports = Park
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -4,19 +4,42 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+  let park;
   beforeEach(function () {
-
+    park = new Park(
+      name='Jurassic Park', 
+      ticket_price=15000, 
+      dinosaur_collection=['Tyrannosaurus', 'Triceratops', 'Velociraptor']
+      );
   })
 
-  it('should have a name');
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park');
+  });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function(){
+    const actual = park.ticket_price;
+    assert.strictEqual(actual, 15000);
+  });
 
-  it('should have a collection of dinosaurs');
 
-  it('should be able to add a dinosaur to its collection');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinosaur_collection;
+    assert.deepStrictEqual(actual, ['Tyrannosaurus', 'Triceratops', 'Velociraptor']);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.add_dinosaur('Stegosaurus');
+    const actual = (park.dinosaur_collection); // could also count length of array
+    assert.deepStrictEqual(actual, ['Tyrannosaurus', 'Triceratops', 'Velociraptor', 'Stegosaurus'])
+  });
+
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.delete_dinosaur('Velociraptor');
+    const actual = (park.dinosaur_collection);
+    assert.deepStrictEqual(actual, ['Tyrannosaurus', 'Triceratops'])
+  });
 
   it('should be able to find the dinosaur that attracts the most visitors');
 
